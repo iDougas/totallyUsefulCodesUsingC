5b7c7c0be5bdc27e6a6f69b2fb00875e66852edd
iDougas <38037625+iDougas@users.noreply.github.com>
2021-10-21 20:01:03 +0300

    Update and rename main.c to alphabet.c

--- alphabet.c
+++ alphabet.c
@@ -2,6 +2,7 @@
 
 int main()
 {
+    // If you don't understand anything what these numbers mean, use the ASCII table (https://www.asciitable.com/) and look for them by decimals
     int a = 65, z = 90;
     while(a <= z)
     {
